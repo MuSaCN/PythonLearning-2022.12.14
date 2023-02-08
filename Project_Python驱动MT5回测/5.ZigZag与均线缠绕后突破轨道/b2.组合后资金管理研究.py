4612719189d55c05171bfb4aeb461a6ce941fc90
MuSa <39754824+MuSaCN@users.noreply.github.com>
2023-02-08 19:11:07 +0800

    update

--- Project_Python驱动MT5回测/5.ZigZag与均线缠绕后突破轨道/b2.组合后资金管理研究.py
+++ Project_Python驱动MT5回测/5.ZigZag与均线缠绕后突破轨道/b2.组合后资金管理研究.py
@@ -105,8 +105,8 @@ def common_set():
 FwdRprAd.bt_reportfolder = FwdRprAd.bt_folder + "\\" + "SplitFund"
 def strategy_set1(): # SplitFund
     myMT5run.input_set("Inp_MM_Mode", "3") # SplitFund
-    myMT5run.input_set("Inp_Lots_IncreDelta", "100||800||100||2400||N")
-    myMT5run.input_set("Inp_Lots_IncreInitLots", "0.01||0.1||0.010000||1.000000||N")
+    myMT5run.input_set("Inp_Lots_IncreDelta", "100||800||100||2400||Y")
+    myMT5run.input_set("Inp_Lots_IncreInitLots", "0.01||0.1||0.010000||1.000000||Y")
 FwdRprAd.prepare(common_set, strategy_set1)
 FwdRprAd.combine_symbol_opt(symbol="USDJPY", optimization=1, deposit=2000)
 
@@ -115,8 +115,8 @@ FwdRprAd.combine_symbol_opt(symbol="USDJPY", optimization=1, deposit=2000)
 FwdRprAd.bt_reportfolder = FwdRprAd.bt_folder + "\\" + "SplitFormula"
 def strategy_set2(): # SplitFormula
     myMT5run.input_set("Inp_MM_Mode", "4") # SplitFormula
-    myMT5run.input_set("Inp_Lots_IncreDelta", "100||400||100||2400||N")
-    myMT5run.input_set("Inp_Lots_IncreInitLots", "0.01||0.1||0.010000||1.000000||N")
+    myMT5run.input_set("Inp_Lots_IncreDelta", "100||400||100||2400||Y")
+    myMT5run.input_set("Inp_Lots_IncreInitLots", "0.01||0.1||0.010000||1.000000||Y")
 FwdRprAd.prepare(common_set, strategy_set2)
 FwdRprAd.combine_symbol_opt(symbol="USDJPY", optimization=1, deposit=2000)
 
